d9081ba766cac83c427b2d7647751d9644c22a08
ElbekIT <qoriyevagavharoy@gmail.com>
2025-12-07 15:02:18 +0500

    add

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1KIzGCvifH-lzNzOtY0F__AL6PU7_B_Y",
-  authDomain: "elbekproductions.firebaseapp.com",
-  databaseURL: "https://elbekproductions-default-rtdb.firebaseio.com",
-  projectId: "elbekproductions",
-  storageBucket: "elbekproductions.firebasestorage.app",
-  messagingSenderId: "467619718063",
-  appId: "1:467619718063:web:f370881cffbb396899c55a",
-  measurementId: "G-Y46P6R2H8V"
+  apiKey: "AIzaSyAGSvG6gqUz198-Y7NMLKq8dnYRmLPE7-o",
+  authDomain: "darian-electronics.firebaseapp.com",
+  databaseURL: "https://darian-electronics-default-rtdb.firebaseio.com",
+  projectId: "darian-electronics",
+  storageBucket: "darian-electronics.firebasestorage.app",
+  messagingSenderId: "439635255126",
+  appId: "1:439635255126:web:e666b5bde36eaaf3ac40f7",
+  measurementId: "G-LXS4VHCQZ7"
 };
 
 // Initialize Firebase
